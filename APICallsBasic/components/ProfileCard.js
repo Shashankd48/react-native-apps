3373cb744e327a9f7ce5dd1b6a36475082f46ac7
Shashank Dubey <jfriday464@gmail.com>
2022-02-06 21:29:07 +0530

    All done

--- APICallsBasic/components/ProfileCard.js
+++ APICallsBasic/components/ProfileCard.js
@@ -1,6 +1,9 @@
 import {Box, Text, Avatar, Heading} from 'native-base';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// const myIcon = ;
 
 const styles = StyleSheet.create({
   container: {
@@ -8,7 +11,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   card: {
-    height: 400,
+    height: 500,
     marginTop: 30,
     backgroundColor: '#fff',
     borderRadius: 10,
@@ -46,7 +49,50 @@ const ProfileCard = ({profile}) => {
             I'am {profile.age} year old
           </Text>
         </Box>
-        <Box></Box>
+
+        <Box style={{paddingVertical: 15}}>
+          <Box
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mb={3}>
+            <Box flex="1" flexDirection="row" alignItems="center">
+              <Box width="30px">
+                <Icon name="phone" size={25} color="#10b981" />
+              </Box>
+              <Heading size="sm">Phone :</Heading>
+            </Box>
+            <Heading size="sm">{profile.phone}</Heading>
+          </Box>
+
+          <Box
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mb={3}>
+            <Box flex="1" flexDirection="row" alignItems="center">
+              <Box width="30px">
+                <Icon name="mobile-phone" size={30} color="#3b82f6" />
+              </Box>
+              <Heading size="sm">Mobile No. :</Heading>
+            </Box>
+            <Heading size="sm">{profile.cell}</Heading>
+          </Box>
+
+          <Box
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mb={3}>
+            <Box flex="1" flexDirection="row" alignItems="center">
+              <Box width="30px">
+                <Icon name="genderless" size={30} color="#a855f7" />
+              </Box>
+              <Heading size="sm">Gender :</Heading>
+            </Box>
+            <Heading size="sm">{profile.gender.toUpperCase()}</Heading>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
